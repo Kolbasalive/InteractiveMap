5da1a22327472ca40620b478bf080a1ec518e795
kolbasalive <karpmakc50@gmail.com>
2023-12-17 12:46:58 +0300

    Init

---
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -0,0 +1,174 @@
+package com.example.navigation
+
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.navigation.ui.theme.NavigationTheme
+import com.example.navigation.ui.theme.ColorViewButton
+import com.example.navigation.ui.theme.ColorViewDark
+import com.example.navigation.ui.theme.ColorViewLine
+
+import java.util.*
+import androidx.compose.runtime.*
+import com.example.navigation.ui.theme.ColorBack
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Red/*MaterialTheme.colorScheme.background*/
+                ) {
+                    OnMap()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun OnMap() {
+    NavigationTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(), color = Color.Red/*MaterialTheme.colorScheme.background*/
+        ) {
+            Map()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Map(modifier: Modifier = Modifier) {
+
+    var webView: WebView? by remember { mutableStateOf(null) }
+
+    val buildPath = remember { mutableStateOf(0) }
+
+    val textStateOn = remember { mutableStateOf("") }
+    val textStateIn = remember { mutableStateOf("") }
+    Column(modifier = Modifier.background(Color.Gray)) {
+        Column(
+            modifier = Modifier
+                .border(2.dp, ColorViewLine)
+                .background(ColorBack)
+        ) {
+            Text(
+                text = "Карта МИИТа",
+                modifier
+                    .align(CenterHorizontally)
+                    .padding(12.dp)
+            )
+            Row(modifier = Modifier.align(CenterHorizontally)) {
+                OutlinedTextField(
+                    value = textStateOn.value,
+                    onValueChange = { textStateOn.value = it },
+                    label = { Text("Откуда") },
+                    modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .padding(4.dp),
+                    colors = TextFieldDefaults.outlinedTextFieldColors( textColor = Color.Black)
+                )
+                OutlinedTextField(
+                    modifier = Modifier
+                        .padding(4.dp),
+                    value = textStateIn.value,
+                    onValueChange = { textStateIn.value = it },
+                    label = { Text("Куда") },
+                    colors = TextFieldDefaults.outlinedTextFieldColors( textColor = Color.Black)
+                )
+            }
+
+            Button(
+                onClick = {
+                    webView?.evaluateJavascript("clearPaths();") {}
+                    var path1 = "781,766 779,789"
+                    if (textStateOn.value == "0") path1 = "1353,1345,1336,1335,1353,1321,1364,1301,1371,1268,1350,1196,1155,1168,888,1134,769,1117,756,1185,729,1180"// От КПП до 6
+
+                    if (textStateIn.value == "5") path1 = "1156,1139,1153,1169,986,1147,889,1134,889,1144,883,1176"// от 1 до 5
+                    if (textStateOn.value == "1" && textStateIn.value == "8") path1 = "1156,1139,1152,1169,889,1134,768,1116,744,1303,735,1298,723,1299,689,1299,657,1302,620,1313,586,1336,547,1361,489,1369,453,1375,427,1373,410,1331,402,1267,404,1218,405,1177,407,1128,405,1016,391,992,376,1006,376,1015"// от 1 до 8
+                    if (textStateIn.value == "") path1 = "781,765,777,790,519,754,504,879,457,878,432,883,408,893,391,906,372,918,349,925,323,927,287,930,284,918,279,901,275,880,272,855,272,828,274,794,276,760"// от 1 до 6
+
+                    // 0 6
+                    // 1 5
+                    // 1 8
+                    val javascriptCode = "click1('$path1');"//
+                    webView?.evaluateJavascript(javascriptCode) {}
+                },
+                colors = ButtonDefaults.buttonColors(ColorViewButton),
+                modifier = Modifier
+                    .padding(vertical = 24.dp)
+                    .align(CenterHorizontally)
+            ) {
+                Text(
+                    "Построить путь",
+                    color = ColorViewDark,
+                    fontSize = 16.sp
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .background(Color.Gray)
+                .fillMaxSize()
+        ) {
+
+            AndroidView(
+                factory = { context ->
+                    WebView(context).apply {
+                        this.webViewClient = WebViewClient()
+                        this.webChromeClient = WebChromeClient()
+                        this.settings.javaScriptEnabled = true
+
+                        this.settings.builtInZoomControls = true
+                        this.settings.displayZoomControls = false
+
+                        webView = this
+
+                        val htmlPath = "file:///android_asset/nav.html"
+                        loadUrl(htmlPath)
+
+                    }
+                },
+                modifier = Modifier.fillMaxSize()
+            )
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/navigation/algos/Dikstra.java
@@ -0,0 +1,251 @@
+package com.example.navigation.algos;
+
+import java.io.*;
+import java.util.*;
+
+public class Dikstra {
+    /**
+     * Количество рёбер графа * 2
+     */
+    private static final int SIZE = 71 * 2;
+    /**
+     * Номер стартового узла
+     */
+    private static int startI = 1;
+
+    private static int startMax = 0;
+    private static int endMax = 0;
+    /**
+     * Номер конечного узла
+     */
+    private static final int endI = 5;
+
+    public static void main(String[] args) {
+
+        int[][] array = new int[SIZE][3];
+
+        HashMap<Integer, Node> graph = createGraph(Objects.requireNonNull(readFile(array)));
+
+        Node start = graph.get(startI);
+        Node end = graph.get(endI);
+        LinkedList<Node> linkedList = getShortestPath(graph,
+                start,
+                end);
+
+        if (linkedList != null) {
+            getNode(linkedList);
+        } else {
+            System.out.println("Пути не существует");
+        }
+
+    }
+
+    /**
+     * Вывод пути
+     */
+    public static void getNode(LinkedList<Node> path) {
+        int sum = 0;
+        for (int i = 0; i < path.size(); i++) {
+            System.out.println(path.get(i).value);
+            if (i != path.size() - 1)
+                sum += path.get(i).parents.get(path.get(i + 1)).weight;
+        }
+        System.out.println("Длина всего пути: " + sum + " дм.");
+    }
+
+    /**
+     * Дейкстра
+     */
+    public static LinkedList<Node> getShortestPath(HashMap<Integer, Node> graph, Node start, Node end) {
+        HashSet<Node> unprocessedNodes = new HashSet<>();
+        HashMap<Node, Integer> timeToNodes = new HashMap<>();
+        initHashTables(start, graph, unprocessedNodes, timeToNodes);
+        calculateTimeToEachNode(unprocessedNodes, timeToNodes);
+        if (timeToNodes.get(end) == null) return null;
+        return getShortestPath(start, end, timeToNodes);
+    }
+
+    public static LinkedList<Node> getShortestPath(Node start, Node end, HashMap<Node, Integer> timeToNodes) {
+        LinkedList<Node> path = new LinkedList<>();
+        Node node = end;
+        while (node != start) {
+            int minTimeToNode = timeToNodes.get(node);
+            path.addFirst(node);
+            for (Map.Entry<Node, Edge> parentAndEdge : node.parents.entrySet()) {
+                Node parent = parentAndEdge.getKey();
+                Edge parentEdge = parentAndEdge.getValue();
+                if (!timeToNodes.containsKey(parent)) continue;
+                boolean prevNodeFound = (parentEdge.weight + timeToNodes.get(parent)) == minTimeToNode;
+                if (prevNodeFound) {
+                    timeToNodes.remove(node);
+                    node = parent;
+                    break;
+                }
+            }
+        }
+        path.addFirst(node);
+        return path;
+    }
+
+    public static void calculateTimeToEachNode(HashSet<Node> unprocessedNodes,
+                                               HashMap<Node, Integer> timeToNodes) {
+        while (!unprocessedNodes.isEmpty()) {
+            Node node = getNodeWithMinTimeTolt(unprocessedNodes, timeToNodes);
+            if (node == null) return;
+            if (timeToNodes.get(node) == Integer.MAX_VALUE) return;
+            for (Edge edge : node.edges) {
+                Node adjacentNode = edge.adjacentNode;
+                if (unprocessedNodes.contains(adjacentNode)) {
+                    int timeToCheck = timeToNodes.get(node) + edge.weight;
+                    if (timeToCheck < timeToNodes.get(adjacentNode))
+                        timeToNodes.put(adjacentNode, timeToCheck);
+                }
+            }
+            unprocessedNodes.remove(node);
+        }
+    }
+
+    public static Node getNodeWithMinTimeTolt(HashSet<Node> unprocessedNodes,
+                                              HashMap<Node, Integer> timeToNodes) {
+        Node nodeWithMinTimeTolt = null;
+
+        int minTime = Integer.MAX_VALUE;
+        for (Node node : unprocessedNodes) {
+            int time = timeToNodes.get(node);
+            if (time < minTime) {
+                minTime = time;
+                nodeWithMinTimeTolt = node;
+            }
+        }
+        return nodeWithMinTimeTolt;
+    }
+
+    /**
+     * Стартовые данные
+     */
+    public static void initHashTables(Node start, HashMap<Integer, Node> graph, HashSet<Node> unprocessedNode,
+                                      HashMap<Node, Integer> timeToNodes) {
+        for (Map.Entry<Integer, Node> mapEntry : graph.entrySet()) {
+            Node node = mapEntry.getValue();
+            unprocessedNode.add(node);
+            timeToNodes.put(node, Integer.MAX_VALUE);
+        }
+        timeToNodes.put(start, 0);
+    }
+
+    /**
+     * Получение или добавление
+     */
+    public static Node addOrGetNode(HashMap<Integer, Node> graph, int value) {
+        if (value == -1) return null;
+        if (graph.containsKey(value)) return graph.get(value);
+
+        Node node = new Node(value);
+        graph.put(value, node);
+        return node;
+    }
+
+    /**
+     * Генерация графа
+     */
+    public static HashMap<Integer, Node> createGraph(int[][] graphData) {
+        HashMap<Integer, Node> graph = new HashMap<>();
+        for (int[] row : graphData) {
+            Node node = addOrGetNode(graph, row[0]);
+            Node adjacentNode = addOrGetNode(graph, row[1]);
+            if (adjacentNode == null) continue;
+
+            Edge edge = new Edge(adjacentNode, row[2]);
+            assert node != null;
+            node.edges.add(edge);
+            adjacentNode.parents.put(node, edge);
+
+        }
+
+        return graph;
+    }
+
+    public static int[][] readFile(int[][] array) {
+        try {
+            File file = new File("C:\\Users\\Max\\navigation\\src\\com\\company\\GraphList");
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+
+            int i = 0;
+            while (line != null) {
+                String[] str = line.replace("--", "")
+                        .replace("  ", " ")
+                        .split(" ");
+
+                if (Objects.equals(str[0], str[1])) {
+                    System.out.println("Неправильные входные данные");
+                    break;
+                }
+
+                int node1 = Integer.parseInt(str[0]);
+                int node2 = Integer.parseInt(str[1]);
+                int weight = getWeight(str[2]);
+
+                array[i][0] = node1;
+                array[i + 1][0] = node2;
+
+                array[i][1] = node2;
+                array[i + 1][1] = node1;
+
+                array[i][2] = weight;
+                array[i + 1][2] = weight;
+
+                if (endMax < node1) endMax = node1;
+                if (startMax < node2) startMax = node1;
+
+                i = i + 2;
+                line = reader.readLine();
+            }
+            chekEndStart();
+            return array;
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+
+    public static int getWeight(String s) {
+        if (s.contains(".")) return Integer.parseInt(s.replace(".", ""));
+        return Integer.parseInt(s) * 10;
+    }
+
+    /**
+     * Сопоставление корпусов(на вход номер корпуса)
+     */
+/*    public static int setBuilding(int number) {
+        return switch (number) {
+            case 2 -> 54;
+            case 3 -> 36;
+            case 4 -> 45;
+            case 5 -> 4;
+            case 6 -> 9;
+            case 7 -> 49;
+            case 8 -> 33;
+            case 9 -> 60;
+            case 10 -> 23;
+            case 11 -> 41;
+            case 12 -> 53;
+            case 13 -> 38;
+            case 14 -> 20;
+            default -> 1;
+        };
+    }*/
+
+
+    public static void chekEndStart(){
+        try {
+            if (endMax < endI || startMax < startI){
+                startI = 1;
+                throw new Exception("No date");
+            }
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
